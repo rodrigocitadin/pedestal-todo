c8d1340b7bba9f6f584d8f83262e309dab383423
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-15 16:59:13 -0300

    create routes

--- src/main.clj
+++ src/main.clj
@@ -9,3 +9,21 @@
 (def ok (partial response  200))
 (def created (partial response  201))
 (def accepted (partial response  202))
+
+(def echo
+  {:name :echo
+   :enter
+   (fn [context]
+     (let [request (:request context)
+           response (ok context)]
+       (assoc context :response response)))})
+
+(def routes
+  (route/expand-routes
+   #{["/todo" :post echo :route-name :list-create]
+     ["/todo" :get echo :route-name :list-query-form]
+     ["/todo/:list-id" :get echo :route-name :list-view]
+     ["/todo/:list-id" :post echo :route-name :list-item-create]
+     ["/todo/:list-id/:item-id" :get echo :route-name :list-item-view]
+     ["/todo/:list-id/:item-id" :put echo :route-name :list-item-update]
+     ["/todo/:list-id/:item-id" :delete echo :route-name :list-item-delete]}))
