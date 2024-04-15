43b8bf3efb5f9b4dd98a587aaa40d039262bfbec
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-15 16:53:41 -0300

    default responses status

--- src/main.clj
+++ src/main.clj
@@ -2,3 +2,10 @@
   (:require [io.pedestal.http :as http]
             [io.pedestal.http.route :as route]
             [io.pedestal.test :as test]))
+
+(defn response [status body & {:as headers}]
+  {:status status :body body :headers headers})
+
+(def ok (partial response  200))
+(def created (partial response  201))
+(def accepted (partial response  202))
