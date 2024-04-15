8046f814dbfec7f8f97bb5d6ac98e01d7b7121e9
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-15 17:01:07 -0300

    wip

--- src/main.clj
+++ src/main.clj
@@ -27,3 +27,28 @@
      ["/todo/:list-id/:item-id" :get echo :route-name :list-item-view]
      ["/todo/:list-id/:item-id" :put echo :route-name :list-item-update]
      ["/todo/:list-id/:item-id" :delete echo :route-name :list-item-delete]}))
+
+(def service-map
+  (http/create-server
+   {::http/routes routes
+    ::http/type :jetty
+    ::http/port 8890}))
+
+(defn start []
+  (http/start (http/create-server service-map)))
+
+;; For interactive development
+(defonce server (atom nil))
+
+(defn start-dev []
+  (reset! server
+          (http/start (http/create-server
+                       (assoc service-map
+                              ::http/join? false)))))
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
